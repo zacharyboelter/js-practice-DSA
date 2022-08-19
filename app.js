27820290d74b20a91d9a627e3f3074af10205c0b
Zachary Boelter <zboelter@gmail.com>
2022-08-19 10:11:59 -0700

    counting sheep

--- app.js
+++ app.js
@@ -128,3 +128,17 @@
 //         return 'Hello guest'
 //     }
 // }
+
+
+
+// Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+function countSheep(num) {
+    let str = ''
+    for (let i = 1; i <= num; i++){
+        str += `${i} sheep...`
+    }
+    return str
+}
+
+console.log(countSheep(10))
