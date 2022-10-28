dabef3b8f47adbfdeb92add20e7f4b57660338c8
Zachary Boelter <zboelter@gmail.com>
2022-10-27 18:54:23 -0700

    count by x (codewars)

--- app.js
+++ app.js
@@ -331,20 +331,48 @@
 //  ~~~~~~~~~~~~~~~~~~~~~ Counting Sheep ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
 
-function countSheep(arrOfSheep){
-    total = 0
-    for (let i = 0; i < arrOfSheep.length; i++){
-        if (arrOfSheep[i] == true){
-            total += 1
-        }
+// function countSheep(arrOfSheep){
+//     total = 0
+//     for (let i = 0; i < arrOfSheep.length; i++){
+//         if (arrOfSheep[i] == true){
+//             total += 1
+//         }
+//     }
+//     return total
+// }
+
+
+// function sheeps(arrayOfSheep){
+//     return arrayOfSheep.filter(Boolean).length
+// }
+
+// console.log(sheeps([true, true, true, false, true]))
+// console.log(countSheep([true, true, true, false, true, true]))
+
+
+// Count by X
+
+// Create a function with two arguments that will return an array of the first n multiples of x.
+
+// Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+// Return the results as an array or list ( depending on language ).
+// Examples
+
+// countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
+// countBy(2,5) === [2,4,6,8,10]
+
+
+const countBy = (x, n) => {
+    const z =[]                             //set empty array
+    for (let i = 1; i <= n; i++){           //loop through n
+        z.push(i * x)                       //multiply the looped by x and push to empty array
     }
-    return total
+    console.log(z)
+    return z                                //return new array
 }
 
-
-function sheeps(arrayOfSheep){
-    return arrayOfSheep.filter(Boolean).length
-}
-
-console.log(sheeps([true, true, true, false, true]))
-console.log(countSheep([true, true, true, false, true, true]))
+countBy(1, 10)
+countBy(10, 100)
+countBy(2, 34)
+countBy(55, 11111)
